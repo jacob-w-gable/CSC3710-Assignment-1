e9a7056446b0a0ec73d820f7c9350fbc224b93a2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-23 00:15:08 -0500

    insert comments, change variable names, slight bug fix

--- cyk.cpp
+++ cyk.cpp
@@ -1,3 +1,17 @@
+
+/**
+ * Author: 	Jacob Gable
+ * File: 	cyk.cpp
+ * Class: 	CSC3710 - Foundations of Computer Science
+ * 
+ * Description:
+ * This is a program that takes two input variables: a test string, 
+ * and a file containing a grammar in Chomsky normal form. It generates 
+ * a table using the CYK algorithm and prints it to the console. If the
+ * first row of the table contains the start symbol, then we know that
+ * the string can be generated using the grammar.
+ */
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,6 +20,7 @@ using namespace std;
 
 int main(int argc, char** argv){
 
+	// bad input detection
 	if(argc != 3){
 		cout << "Please run the program with two parameters:" << endl;
 		cout << "./CYK.exe <language-file> <test-string>" << endl;
@@ -13,17 +28,13 @@ int main(int argc, char** argv){
 		cout << "./CYK.exe language.txt aabbb" << endl;
 		return 1;
 	}
-
-	//cout << "Input file name: ";
-	//string filename;
-	//getline(cin, filename);
-	string filename = argv[1];
-
-	ifstream infile(filename);
 	
+	string filename = argv[1];
+	ifstream infile(filename);
+
+	// read the input file into char** rules.
 	char** rules;
 	int ruleCount;
-	
 	if(infile.good()){
 		infile >> ruleCount;
 		infile.ignore();
@@ -31,55 +42,76 @@ int main(int argc, char** argv){
 		for(int i = 0; i < ruleCount; i++){
 			string line;
 			getline(infile, line);
+
+			// each char* is of length 3, converting the format A->BC to the array {A, B, C}.
+			// a space character (or no character at all) effectively acts as the null-character lambda
 			rules[i] = new char[3];
 			rules[i][0] = line[0];
-			rules[i][1] = line[3];
+			if(line.length() > 3){
+				rules[i][1] = line[3];
+			} else {
+				rules[i][1] = ' ';
+			}
 			if(line.length() > 4){
 				rules[i][2] = line[4];
 			} else {
 				rules[i][2] = ' ';
 			}
 		}
+
 	} else {
 		cout << "Failed to open the given file." << endl;
 		return 1;
 	}
-	
-	//string inputString;
-	//cout << "Enter test string: ";
-	//getline(cin, inputString);
+
 	string inputString = argv[2];
+	int stringLength = inputString.length();
 	
-	int n = inputString.length();
+	// CYK Step 1: Initialize an empty matrix
+	// index = row*stringLength + column
+	string* matrix = new string[stringLength*stringLength];
 	
-	// [column][row]
-	// column + n*row
-	string* matrix = new string[n*n];
-	
-	for(int i = 0; i < n; i++){
+	// CYK Step 2: Initialize diagonal of the matrix
+	for(int i = 0; i < stringLength; i++){
 		for(int rule = 0; rule < ruleCount; rule++){
+			// Find the terminating rules that are valid for each index of the test string, 
+			// and put them in the corresponding (index,index) slot in the diagonal of the matrix.
 			if(rules[rule][1] == inputString[i] && rules[rule][2] == ' '){
-				matrix[n*i+i] += rules[rule][0];
+				matrix[stringLength*i+i] += rules[rule][0];
 			}
 		}
 	}
 	
-	for(int step = 2; step <= n; step++){
-		for(int i = 1; i <= n-step+1; i++){
+	// CYK Step 3: Complete matrix elements above the diagonal.
+	// The outermost three for-loops iterate through each element in the matrix 
+	// that are above the diagonal.
+	// The current element is represented as "(row, column) = (i-1, i+step-2)"
+	for(int step = 2; step <= stringLength; step++){
+		for(int i = 1; i <= stringLength-step+1; i++){
 			for(int k = i; k <= i+step-2; k++){
+				// convert "step", "i", and "k" to row/column coordinates to simplify code
 				int row1 = i-1;
 				int col1 = k-1;
 				int row2 = k;
 				int col2 = i+step-2;
-				string pro1 = matrix[n*row1+col1];
-				string pro2 = matrix[n*row2+col2];
+
+				// Get two elements relative to the main element in question. They will be
+				// compared together.
+				string pro1 = matrix[stringLength*row1+col1];
+				string pro2 = matrix[stringLength*row2+col2];
+
+				// given symbol A as a character in "pro1",
+				//   and symbol B as a character in "pro2"...
 				for(int a = 0; a < pro1.length(); a++){
 					for(int b = 0; b < pro2.length(); b++){
+						// ... check to see if there is a rule C,
+						// such that C->AB
 						for(int c = 0; c < ruleCount; c++){
 							if(pro1[a] == rules[c][1] && pro2[b] == rules[c][2]){
+								// if so, add the rule C to the list at the current element of the matrix
 								int destRow = i-1;
 								int destCol = i+step-2;
-								matrix[n*destRow+destCol] += rules[c][0];
+								matrix[stringLength*destRow+destCol] += rules[c][0];
 							}
 						}
 					}
@@ -87,10 +119,13 @@ int main(int argc, char** argv){
 			}
 		}
 	}
+
+	// CYK algorithm complete, matrix generated. Note that the area under the diagonal does not get used.
 	
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			string chars = matrix[n*i+j];
+	// Print each element in a fancy way
+	for(int i = 0; i < stringLength; i++){
+		for(int j = 0; j < stringLength; j++){
+			string chars = matrix[stringLength*i+j];
 			string element = "{";
 			if(chars.length() > 1){
 				for(int k = 0; k < chars.length()-1; k++){
@@ -106,7 +141,8 @@ int main(int argc, char** argv){
 		cout << "\n";
 	}
 	
-	for(int i = 0; i < n; i++){
+	// Memory management
+	for(int i = 0; i < ruleCount; i++){
 		delete [] rules[i];
 	}
 	delete [] rules;
